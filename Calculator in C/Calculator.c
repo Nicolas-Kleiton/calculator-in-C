03108df55f4a9cdc2d6ce18b8ae9bf6afd91d62e
Nicolas Kleiton <inicolaskleiton@gmail.com>
2024-07-12 20:52:16 -0300

    calculator in c

---
+++ Calculator in C/Calculator.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+
+   float num1, num2;
+   char operation;
+
+   printf("Choose an operation (+,-,*,/): ");
+   scanf("%c", &operation);
+   printf("\nEnter the first number: ");
+   scanf("%f", &num1);
+   printf("\nEnter the second number: ");
+   scanf("%f",&num2);
+ 
+   switch (operation)
+   {
+    case '+': 
+        printf("\nResult: %.2f", num1+num2);
+        break;
+    case '-': 
+        printf("\nResult: %.2f", num1-num2);
+        break;
+    case '*': 
+        printf("\nResult: %.2f", num1*num2);
+        break;
+    case '/':
+        if (num2 == 0) {
+            printf("\nCannot divide by 0");
+        } else {
+        printf("\nResult: %.2f", num1/num2);
+        }
+        break;
+   default: 
+        printf("\nInvalid operation, try again.");
+   }
+
+	return 0;
+}
+
