c696dc7c2efd0fce4927d47f8dcff4b543eb03fe
Nicolas Kleiton <inicolaskleiton@gmail.com>
2024-07-30 20:20:36 -0300

    pequena alteração

--- Calculator in C/Calculator.c
+++ Calculator in C/Calculator.c
@@ -7,31 +7,31 @@ int main() {
 
    printf("Choose an operation (+,-,*,/): ");
    scanf("%c", &operation);
-   printf("\nEnter the first number: ");
+   printf("Enter the first number: ");
    scanf("%f", &num1);
-   printf("\nEnter the second number: ");
+   printf("Enter the second number: ");
    scanf("%f",&num2);
  
    switch (operation)
    {
     case '+': 
-        printf("\nResult: %.2f", num1+num2);
+        printf("Result: %.2f\n", num1+num2);
         break;
     case '-': 
-        printf("\nResult: %.2f", num1-num2);
+        printf("Result: %.2f\n", num1-num2);
         break;
     case '*': 
-        printf("\nResult: %.2f", num1*num2);
+        printf("Result: %.2f\n", num1*num2);
         break;
     case '/':
         if (num2 == 0) {
-            printf("\nCannot divide by 0");
+            printf("Cannot divide by 0\n");
         } else {
-            printf("\nResult: %.2f", num1/num2);
+            printf("Result: %.2f\n", num1/num2);
         }
         break;
    default: 
-        printf("\nInvalid operation, try again.");
+        printf("Invalid operation, try again.\n");
    }
 
 	return 0;
